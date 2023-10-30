5442f38914cd2e5829585cb0d3e24f36342b1e06
taula <matratau000@gmail.com>
2023-10-30 07:02:40 -0500

    final commit

--- main.py
+++ main.py
@@ -7,27 +7,28 @@ import os
 from pydub import AudioSegment
 import whisper
 
+# Bypass SSL verification for HTTPS requests (use with caution)
 ssl._create_default_https_context = ssl._create_unverified_context
 
 
-# Now you can proceed to use urllib or any other library that fetches data over HTTPS
-
-
 def convert_audio(file_path):
+    """Convert audio to mono WAV format."""
     audio = AudioSegment.from_file(file_path)
-    audio = audio.set_channels(1)  # Ensure mono audio
+    audio = audio.set_channels(1)
     wav_path = "temp.wav"
     audio.export(wav_path, format="wav")
     return wav_path
 
 
 def whisper_transcribe(audio_path):
-    model = whisper.load_model("small")  # Change the model size as per your requirement
+    """Transcribe audio using the whisper model."""
+    model = whisper.load_model("small")
     result = model.transcribe(audio_path)
     return result["text"]
 
 
 def save_transcription(transcription):
+    """Save transcription as TXT and JSON."""
     if not os.path.exists('transcripts'):
         os.makedirs('transcripts')
     with open('transcripts/transcription.txt', 'w') as txt_file:
@@ -37,6 +38,7 @@ def save_transcription(transcription):
 
 
 def on_upload():
+    """Upload audio, transcribe, display and save transcription."""
     file_path = filedialog.askopenfilename()
     if file_path:
         wav_path = convert_audio(file_path)
@@ -47,13 +49,17 @@ def on_upload():
             save_transcription(transcription)
 
 
+# Initialize tkinter window
 root = tk.Tk()
 root.title("Audio Transcription App")
 
+# Add "Upload Audio File" button
 upload_btn = tk.Button(root, text="Upload Audio File", command=on_upload)
 upload_btn.pack()
 
+# Add display area for transcription
 transcription_display = tk.Text(root)
 transcription_display.pack()
 
+# Start tkinter loop
 root.mainloop()
